9bfbdd7a4805d4ec56d454b97cee0de03905fb8c
dam <dam@203P20>
2022-02-04 11:49:22 +0100

    primer commit

---
+++ src/refactorizar/Clase1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refactorizar;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String mensaje="La dirección de mi repositorio remoto para este ejercicio es https://github.com/Alvarorsp18/CONTROLRAMOS";
+        System.out.println(mensaje);
+    }
+    
+}
