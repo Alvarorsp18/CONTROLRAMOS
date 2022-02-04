6d63f6f4bf28acf2d014a65fdd986e68bfe4c343
dam <dam@203P20>
2022-02-04 12:02:08 +0100

    segundo commit

---
+++ src/refactorizar/Clase2.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refactorizar;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    private String mensaje2="Este es el mensaje2 de la clase2, creada en la rama PRODUCCION.";
+    private int edad=18;
+}
